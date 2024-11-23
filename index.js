bf5ec0064d0bcfdde5fa82bdf6e97d3a2dc0e2af
Евгения Кабацкая <109961605+Engy2228@users.noreply.github.com>
2024-11-23 14:21:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const addZero = (number) => String(number).length === 1 ? `0${number}` : String(number)
+
+const getDateFormat = (date, separator ='.') => {
+  if (!(date instanceof Date)) {
+    return 'Первый параметр должен быть экземпляром класса Date!'}
+
+    const todayMs = date.getDate()
+    const monthIndex = date.getMonth()
+    const year = date.getFullYear()
+    return [todayMs, monthIndex +1, year].map(addZero).join(separator)
+    
+}
+console.log(getDateFormat(new Date('5, 5, 2024')))
+
+
+
